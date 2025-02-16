704e2de793b4aed9799b185aa8f8c5f2c5f9be9b
LawrenceWang <lwang07@berkeley.edu>
2025-02-15 16:44:43 -0800

    fixed SQLAlchemy RuntimeError

--- src/models.py
+++ src/models.py
@@ -5,18 +5,18 @@ from database import db
 class Event(db.Model):
     __tablename__ = "event"
     
-    id = mapped_column(Integer, primary_key=True)
-    latitude = mapped_column(types.Float())
-    longitude = mapped_column(types.Float())
+    id = mapped_column(db.Integer, primary_key=True)
+    latitude = mapped_column(db.Float)
+    longitude = mapped_column(db.Float)
 
-    city = mapped_column(String())
-    state = mapped_column(String(10))
-    country = mapped_column(String(50))
+    city = mapped_column(db.String)
+    state = mapped_column(db.String(10))
+    country = mapped_column(db.String(50))
     
-    title = mapped_column(String())
-    content = mapped_column(String())
-    category = mapped_column(String(30))
+    title = mapped_column(db.String)
+    content = mapped_column(db.String)
+    category = mapped_column(db.String(30))
     date = mapped_column(types.Date())
-    link = mapped_column(String())
+    link = mapped_column(db.String)
 
     
