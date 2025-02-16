484bce59155a55337cada1d67d5a7f9a3ae9fe18
LawrenceWang <lwang07@berkeley.edu>
2025-02-15 23:44:53 -0800

    updated chat.py for date, location, and title

--- src/chat.py
+++ src/chat.py
@@ -14,6 +14,48 @@ def news_summary(api_key, url):
     
     return completion.choices[0].message
 
+def get_date(api_key, url):
+    client = OpenAI(api_key="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")
+    
+    
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {f"article publication date mm-dd-yy: {url}"}
+        ]
+    )
+    
+    return completion.choices[0].message
+
+def city_country(api_key, url):
+    client = OpenAI(api_key="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")
+    
+    
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {f"return only city, country: {url}"}
+        ]
+    )
+    
+    return completion.choices[0].message
+
+def article_title(api_key, url):
+    client = OpenAI(api_key="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")
+    
+    
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        store=True,
+        messages=[
+            {f"only article title in quotes: {url}"}
+        ]
+    )
+    
+    return completion.choices[0].message
+
 # def test_openai(api_key):
 #     client = OpenAI(api_key=api_key)
 #     try:
