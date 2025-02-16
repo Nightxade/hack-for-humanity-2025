9fd152d0585f18958247e08433c7afaabbcceb61
LawrenceWang <lwang07@berkeley.edu>
2025-02-15 22:54:35 -0800

    finished scrape_websites to find links and their topics

--- src/scraper.py
+++ src/scraper.py
@@ -1,13 +1,23 @@
-from requests_html import HTMLSession
-session = HTMLSession()
+import requests
+from bs4 import BeautifulSoup
 
-topics = ["humanitarian", "human%20rights", "health", "disease", "conflict", "war", "environmental"]
-area = ["global", "united%20states", "bay%20area"]
-links = []
 
-for i in topics:
-    for t in area:
-        url = f'https://news.google.com/search?q={i}%20{t}%20news&hl=en-US&gl=US&ceid=US%3Aen'
-        r = session.get(url)
-        r.html.render(sleep = 1)
-        links += r.html.find('')
+def scrape_websites():
+    topics = ["humanitarian", "human%20rights", "health", "disease", "conflict", "war", "environmental"]
+    area = ["global", "united%20states", "bay%20area"]
+    all_links = {}
+
+    for i in topics:
+        for t in area:
+            url = f'https://news.google.com/search?q={i}%20{t}%20news&hl=en-US&gl=US&ceid=US%3Aen'
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            temp = 0
+            for item in soup.find_all('a', class_="WwrzSb"):
+                all_links['news.google.com' + item['href'][1:]] = i
+                if temp == 5:
+                    break
+                else:
+                    temp += 1
+
+    return list(all_links.keys())
