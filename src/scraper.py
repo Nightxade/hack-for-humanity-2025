bce0f9edb206d538e34703b04e3beabefa08f1fa
LawrenceWang <lwang07@berkeley.edu>
2025-02-15 22:00:53 -0800

    started working on the scraper

--- src/scraper.py
+++ src/scraper.py
@@ -1,37 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+session = HTMLSession()
 
-urls = [
-    "https://www.cnn.com/us/crime-and-justice",
-    "https://www.bbc.com/news/topics/cyqpkren475t",
-    "https://www.bbc.com/news/topics/cwlw3xz0115t",
-    "https://www.bbc.com/news/topics/c6lpr12gep3t",
-    "https://www.wsj.com/news/collection/corporate-social-responsibility-report-de7b3d79"
-]
+topics = ["humanitarian", "human%20rights", "health", "disease", "conflict", "war", "environmental"]
+area = ["global", "united%20states", "bay%20area"]
+links = []
 
-def get_article_text(article_url):
-    response = requests.get(article_url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        paragraphs = soup.find_all("p")
-        return "\n".join([p.get_text(strip=True) for p in paragraphs])
-    return "Failed to retrieve article text"
-
-for url in urls:
-    print(f"Scraping: {url}")
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "html.parser")
-        
-        articles = soup.find_all("a", href=True)
-        article_links = [a["href"] for a in articles if "article" in a["href"]]
-        
-        print(f"Article texts from {url}:")
-        for idx, article_link in enumerate(article_links[:10], start=1): # limit 10 articles
-            full_url = article_link if article_link.startswith("http") else url + article_link
-            print(f"\nArticle {idx}: {full_url}")
-            print(get_article_text(full_url))
-        print("\n" + "-"*100 + "\n") # limit 100 lines
-    else:
-        print(f"Failed to retrieve {url}")
+for i in topics:
+    for t in area:
+        url = f'https://news.google.com/search?q={i}%20{t}%20news&hl=en-US&gl=US&ceid=US%3Aen'
+        r = session.get(url)
+        r.html.render(sleep = 1)
+        links += r.html.find('')
