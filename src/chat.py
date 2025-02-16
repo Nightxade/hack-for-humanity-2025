abab0fe03fd36db973b3d52b6e7b5bb952532fa3
LawrenceWang <lwang07@berkeley.edu>
2025-02-16 01:50:30 -0800

    finalized chat.py

--- src/chat.py
+++ src/chat.py
@@ -1,14 +1,14 @@
 from openai import OpenAI
 
-def article_summary(api_key, url):
-    client = OpenAI(api_key="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")
+def article_summary(apikey, url):
+    client = OpenAI(api_key=apikey)
     
     
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         store=True,
         messages=[
-            {f"Format all following data in JSON.\n\nKey: date, Value: the date of publication in mm-dd-yy\nKey: city, Value: the city of event\nKey:country, Value: country of city\nKey:latitude, Value: latitude of city\nKey:longitude, Value:longitude of city\nKey: article title, Value: article title in quotes\nKey: summary, Value: 3-4 sentence summary focus on context and impact \n{url}"}
+            {"role": "user", "content": f"Format all following data in JSON.\n\nKey: date, Value: the date of publication in mm-dd-yy\nKey: city, Value: the city of event\nKey:country, Value: country of city\nKey:latitude, Value: latitude of city (5 decimals)\nKey:longitude, Value:longitude of city (5 decimals)\nKey: article title, Value: article title in quotes\nKey: summary, Value: 3-4 sentence summary focus on context and impact \n{url}"}
         ]
     )
     
